bc6548e7faa18701274c94c9821ec3a69edf73c7
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 05:08:24 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,10 @@
 class Backer
   
-  attr_accessor :backed_projects
+  attr_accessor :name, :backed_projects
   
+  def initialize(name, backed_projects)
+    @name = name
+    @backed_projects = []
+  end
   
 end 
