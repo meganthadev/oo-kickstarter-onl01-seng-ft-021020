1ec88867afe9476c75ffbcf15fe9bbc7674cbb52
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 05:20:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
 class Backer
   
-  attr_accessor :name, :backed_projects
-
+  attr_accessor :backed_projects
+  attr_reader :name
+  
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-
-  def back_project(project)
-    @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(self)
-  end
-end
+  
+  def backed_projects(project)
+    backed_projects << project
+    project.backers << self  
+  end   
+  
+end 
